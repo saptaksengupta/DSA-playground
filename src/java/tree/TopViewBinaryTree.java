ed6d0e2493f1c88b445b88aa9d648fbde8b129c2
saptaksengupta <deeps.sengupta>
2022-07-14 00:20:09 +0530

    solved: top view traversal of a binary tree

---
+++ src/java/tree/TopViewBinaryTree.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+
+class Touple {
+    TreeNode node;
+    int row;
+
+    public Touple(TreeNode _node, int _row) {
+        this.node = _node;
+        this.row = _row;
+    }
+}
+
+class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class TopViewBinaryTree {
+    
+    private static List<Integer> getTopView(TreeNode root) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        Queue<Touple> q = new LinkedList<>();
+        q.offer(new Touple(root, 0));
+        while(!q.isEmpty()) {
+            Touple current = q.poll();
+            TreeNode cNode = current.node;
+            int x = current.row;
+
+            if (!map.containsKey(x)) {
+                map.put(x, cNode.value);
+            }
+
+            if (null != cNode.left) {
+                q.offer(new Touple(cNode.left, x - 1));
+            }
+
+            if (null != cNode.right) {
+                q.offer(new Touple(cNode.right, x + 1));
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> vertical : map.entrySet()) {
+            list.add(vertical.getValue());
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        TreeNode tr = new TreeNode(1);
+
+        tr.left = new TreeNode(2);
+        tr.left.left = new TreeNode(4);
+        tr.left.right = new TreeNode(5);
+        tr.left.right.left = new TreeNode(6);
+
+        tr.right = new TreeNode(3);
+        tr.right.right = new TreeNode(7);
+        System.out.print(TopViewBinaryTree.getTopView(tr));
+    }
+}
